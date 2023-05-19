716b08670b399e60e94ac9c07eeaec9d45d63a1c
web-sujal <web.sujal@gmail.com>
2023-05-19 23:05:38 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl text-red-600 font-bold underline">
+      Hello ja ;world!
+    </h1>
+  );
+}
+
+export default App;
